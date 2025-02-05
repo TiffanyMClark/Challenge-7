8d4f85927db0a83c2a9e4396259d998d92239c9d
tiffany clark <tiffanymarie9811@gmail.com>
2025-02-05 12:02:49 -0500

    added badges hopefully?

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -18,7 +18,7 @@ const questions = [
   },
   {
     type: "input",
-    name: "Installiation",
+    name: "Installation",
     message: "What do you need to do to install your project?",
   },
   {
@@ -27,9 +27,15 @@ const questions = [
     message: "List your collaborators if any.",
   },
   {
-    type: "input",
+    type: "list",
     name: "License",
     message: "What License will we be using?",
+    choices: [
+      "MIT",
+      "Mozilla Public License 2.0",
+      "GNU GPL v3",
+      "Not Listed, you can change it later",
+    ],
   },
   {
     type: "input",
@@ -46,6 +52,15 @@ const questions = [
 // TODO: Create a function to write README file
 
 function generateMarkdown(data) {
+  let licenseBadge = "";
+  if ((data, License === "MIT")) {
+    ("[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)");
+  } else if (data.License === "Mozilla Public License 2.0") {
+    ("[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)");
+  } else if (data.License === "GNU GPL v3") {
+    ("[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)");
+  }
+
   return `
 
     # Title
@@ -56,8 +71,8 @@ function generateMarkdown(data) {
     ${data.Description}
 
 
-    ## Installiation
-     ${data.Installiation}
+    ## Installation
+     ${data.Installation}
 
 
     ## Credits
@@ -93,3 +108,28 @@ function init() {
 
 // Function call to initialize app
 init();
+
+// read me acceptance for challenge ***********
+//WHEN I am prompted for information about my application repository
+
+//THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+
+// WHEN I choose a license for my application from a list of options
+
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+
+// WHEN I enter my GitHub username
+
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+
+// WHEN I enter my email address
+
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+
+// WHEN I click on the links in the Table of Contents
+
+// THEN I am taken to the corresponding section of the README
